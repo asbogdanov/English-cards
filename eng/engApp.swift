e46b59e3a78d1ace8a6b2d03885e21acec2f7e18
Aleksandr Bogdanov <basik0204@gmail.com>
2023-04-19 19:10:40 +0300

    Initial Commit

---
+++ eng/engApp.swift
@@ -0,0 +1,17 @@
+//
+//  engApp.swift
+//  eng
+//
+//  Created by Aleksandr Bogdanov on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct engApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
