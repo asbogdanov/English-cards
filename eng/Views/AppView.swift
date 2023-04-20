d86408c1ec3f6a7f4276c59d56acc36daca072d4
Aleksandr Bogdanov <basik0204@gmail.com>
2023-04-20 14:28:19 +0300

    Tab Bar View with two buttons

--- eng/Views/AppView.swift
+++ eng/Views/AppView.swift
@@ -8,13 +8,80 @@
 import SwiftUI
 
 struct AppView: View {
+    @State var selectedIndex = 0
+
+    let icons = [
+        "house",
+        "list.bullet.clipboard",
+//        "person"
+    ]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 0) {
+            //Content
+            ZStack {
+                switch selectedIndex {
+                case 0:
+                    NavigationView {
+                        VStack {
+                            Text("First screen")
+                        }
+                        .navigationTitle("Home")
+                    }
+                case 1:
+                    NavigationView {
+                        VStack {
+                            Text("First screen")
+                        }
+                        .navigationTitle("To study")
+                    }
+//                case 2:
+//                    NavigationView {
+//                        VStack {
+//                            Text("First screen")
+//                        }
+//                        .navigationTitle("Profile")
+//                    }
+                default:
+                    NavigationView {
+                        VStack {
+                            Text("Second screen")
+                        }
+                        .navigationTitle("Second")
+                    }
+                }
+            }
+
+            Spacer()
+
+            Divider()
+                .padding(.bottom, 20)
+            HStack{
+                ForEach(0..<2, id: \.self) { number in
+                    Spacer()
+                    Button(action: {
+                        self.selectedIndex = number
+                    }, label: {
+                        Image(systemName: icons[number])
+                            .font(.system(
+                                size: 25,
+                                weight: .regular,
+                                design: .default))
+                            .foregroundColor(selectedIndex == number ? Color(.label) : Color(UIColor.lightGray))
+                    })
+                    Spacer()
+                }
+            }
+        }
     }
 }
+
+
 
 struct AppView_Previews: PreviewProvider {
     static var previews: some View {
         AppView()
     }
 }
+
+
--- eng/engApp.swift
+++ eng/engApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct engApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView()
         }
     }
 }
