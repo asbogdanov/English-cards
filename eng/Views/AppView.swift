cfd7e1ac57619880769ec6cc9d8a341e802b18f4
Aleksandr Bogdanov <basik0204@gmail.com>
2023-04-20 14:27:26 +0300

    Tab Bar View with two buttons

---
+++ eng/Views/AppView.swift
@@ -0,0 +1,20 @@
+//
+//  AppView.swift
+//  eng
+//
+//  Created by Aleksandr Bogdanov on 19.04.2023.
+//
+
+import SwiftUI
+
+struct AppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppView()
+    }
+}
